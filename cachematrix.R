c28ec3d2342b786c4b00ced61ab25a9652bc42a4
PauM01 <paulmarkert@mchsi.com>
2015-07-23 22:16:24 -0400

    Updated with final version of functions for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## function:    makeCacheMatrix 
+## description: This function will hold getters and setters for storing a matrix and its corresponding inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
   storedInverseMatrix <- NULL
   
-  set <- function(y) {
-    x <- y
-    storedInverseMatrix <<- NULL
+  # function:     set
+  # description:  stores the input parameter into the local variable 'x'. Assumes 'x' is a square matrix. Also resets
+  #               the corresponding inverted matrix to null.
+  set <- function(newMatrix) {
+    if (!identical(newMatrix,x)) {
+      x <<- newMatrix
+      storedInverseMatrix <<- NULL
+    }
   }
   
-  
+  # function:     get
+  # description:  returns the matrix stored in local variable 'x'.
   get <- function() {
-    x
+    return(x)
   }
   
+  # function:     setInverseMatrix
+  # description:  stores the input parameter into the local variable 'storedInverseMatrix'. Assumes 'newInverseMatrix'
+  #               is a square matrix and is the inverse matrix for 'x'.
   setInverseMatrix <- function(newInverseMatrix) {
     storedInverseMatrix <<- newInverseMatrix
   }
   
+  # function:     getInverseMatrix
+  # description:  returns the matrix stored in local variable 'storedInverseMatrix'
   getInverseMatrix <- function () {
     return(storedInverseMatrix)
   }
@@ -28,19 +37,35 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## function:    cacheSolve 
+## description: This function will accept a list of type makeCacheMatrix and will look in the 
+##              environment to see if the cache has an inverse matrix stored for that object. If
+##              there is an inverse matrix then this function will return it to the caller. If there
+##              is no inverse matrix then this function will calculate the inverse matrix, store it and return
+#               it to the caller.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  m <- x$getInverseMatrix()
+  ## Return a matrix that is the inverse of 'x'
+  tempInverse <- x$getInverseMatrix()
   
-  if(!is.null(m)) {
+  #1) Check to see if a value was returned. If so, return that value to the caller.
+  if(!is.null(tempInverse)) {
     message("getting cached inverseMatrix")
-    return(m)
+    return(tempInverse)
   }
   
-  data <- x$getMatrix()
-  m <- solve(data)
+  #2) Assert - no value has been returned. Call the solve() function to determine the inverse value for
+  #            the matrix stored in the object that was passed to this function.
+  data <- x$get()
+  tempInverse <- solve(data)
   
-  x$setInverseMatrix(m)
+  #3) Store the inverse matrix in the object
+  x$setInverseMatrix(tempInverse)
+  
+  #4) Return the inverted matrix to the caller
+  message("calculated cached invertMatrix")
+  return(tempInverse)
 }
+
+
